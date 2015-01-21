14feafb39ad436452d9f52a5922cdf21b340d3bf
XI316214 <XI316214@IS28925X316214S.scisb.isban.corp>
2015-01-21 17:32:39 +0100

    Menu Script
    Signed-off-by: XI316214 <XI316214@IS28925X316214S.scisb.isban.corp>

--- e-FX/SCR/installSW.sh
+++ e-FX/SCR/installSW.sh
@@ -9,51 +9,106 @@
 #
 # Author: Ramon Martin Lopez [ramn.martn@servexternos.isban.es]
 # Since: 19/01/2015 
-# Last Modified: 20/01/2015 (ramn.martn)
+# Last Modified: 21/01/2015 (ramn.martn)
 #
 ###############################################################################
 
 EFX_INSTALLER_HOME=/home/efxbuild/UploadToSatellite
+EFX_INSTALLER_LOG_FILE=$EFX_INSTALLER_HOME/installSW.log
+EFX_INSTALLER_ERROR_FILE=$EFX_INSTALLER_HOME/installSW.error
 
 export $EFX_INSTALLER_HOME
-source  $EFX_INSTALLER_HOME/uploadSW2Satellite.sh
+export $EFX_INSTALLER_LOG_FILE
+export $EFX_INSTALLER_ERROR_FILE
+source $EFX_INSTALLER_HOME/uploadSW2Satellite.sh
 
-function showMenu {
-	echo -e "##########################################"
-	echo "# Welcome to the EFX Installation Program"
-	echo -e "##########################################"
-	PS3='Please enter your choice: '
-	options=("Install Baxter" "Install Baxter Air" "Install Cerebro" "Install Caplin" "Quit")
-	select opt in "${options[@]}"
-	do
-	    case $opt in
-	        "Install Baxter")
-	            echo "you chose to Install new Baxter release"
-	            upload2Satellite Baxter
-	            ;;
-	        "Install Baxter Air")
-	            echo "you chose to Install new Baxter release"
-	            upload2Satellite Baxter_Air
-	            ;;    
-	        "Install Cerebro")
-	            echo "you chose to Install new Cerebro release"
-				upload2Satellite Cerebro
-	            ;;
-	        "Install Caplin")
-	            echo "you chose to Install new Caplin release"
-	            upload2Satellite Caplin
-	            ;;
-	        "Quit")
-	            break
-	            ;;
-	        *) echo invalid option;;
-	    esac
-	done
+#Main menu
+showMenu(){
+    NORMAL=`echo "\033[m"`
+    MENU=`echo "\033[36m"` #Blue
+    NUMBER=`echo "\033[33m"` #yellow
+    FGRED=`echo "\033[41m"`
+    RED_TEXT=`echo "\033[31m"`
+    ENTER_LINE=`echo "\033[33m"`
+    clear
+    echo -e "${MENU}#############################################${NORMAL}"
+    echo -e "${MENU}# Welcome to the EFX Installation Program${NORMAL}"
+    echo -e "${MENU}#############################################${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 1)${MENU} Install Baxter ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 2)${MENU} Install Baxter Air ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 3)${MENU} Install Cerebro ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 4)${MENU} Install Caplin ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 5)${MENU} ${NORMAL}"
+    echo -e "${MENU}*********************************************${NORMAL}"
+    echo -e "${ENTER_LINE}Please enter a menu option and enter or ${RED_TEXT}enter to exit. ${NORMAL}"
+    read opt
+}
+
+#Show option picked
+function option_picked() {
+    COLOR='\033[01;31m' # bold red
+    RESET='\033[00;00m' # normal white
+    MESSAGE=${@:-"${RESET}Error: No message passed"}
+    echo -e "${COLOR}${MESSAGE}${RESET}"
 }
 
 #wget  [option]  [URL]
 
-clear
+# Clear installation log & error
+rm $EFX_INSTALLER_LOG_FILE
+rm $EFX_INSTALLER_ERROR_FILE
+
+# Show Menu
 showMenu
+
+while [ opt != '' ]
+    do
+    if [[ $opt = "" ]]; then 
+            exit;
+    else
+        case $opt in
+        1) clear
+        	option_picked "you chose to Install new Baxter release"
+        	upload2Satellite Baxter 2>$EFX_INSTALLER_ERROR_FILE
+        	sleep 2
+        	showMenu
+        	;;
+
+        2) clear
+            option_picked "you chose to Install new Baxter Air release"
+            upload2Satellite Baxter_Air 2>$EFX_INSTALLER_ERROR_FILE
+            sleep 2
+        	showMenu
+            ;;
+
+        3) clear
+            option_picked "you chose to Install new Cerebro release"
+        	upload2Satellite Cerebro 2>$EFX_INSTALLER_ERROR_FILE
+        	sleep 2
+            showMenu
+            ;;
+
+        4) clear
+            option_picked "you chose to Install new Caplin release"
+        	upload2Satellite Caplin 2>$EFX_INSTALLER_ERROR_FILE
+        	sleep 2
+            showMenu
+            ;;
+
+        x) clear
+        	exit
+        	;;
+
+        \n) clear
+        	exit
+        	;;
+
+        *) clear
+        	option_picked "Pick an option from the menu";
+        	showMenu
+        	;;
+    	esac
+	fi
+done
 
 
--- e-FX/SCR/uploadSW2Satellite.sh
+++ e-FX/SCR/uploadSW2Satellite.sh
@@ -9,21 +9,25 @@
 #
 # Author: Ramon Martin Lopez [ramn.martn@servexternos.isban.es]
 # Since: 19/01/2015 
-# Last Modified: 20/01/2015 (ramn.martn)
+# Last Modified: 21/01/2015 (ramn.martn)
 #
 ###############################################################################
 
 # Uploads a SW Release To Satellite Channels
-function upload2Satellite { 
+function upload2Satellite() { 
 
+	UPLOAD_AREA_FOLDER="/home/efxbuild/upload_area/"
+
+	# Read Release Number from user input
+	echo -e "Choose a release from one of the following in $UPLOAD_AREA_FOLDER	\n"
+	find /home/efxbuild/upload_area -type d | grep $1
 	echo -n "Please introduce release number, Ej.: 3.4.29 > "
 	read RELEASE_NUMBER
 	
-	UPLOAD_AREA="/home/efxbuild/upload_area/"
 	#APPLICATION=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 	APPLICATION=$1
 	SW_RELEASE_FOLDER=$APPLICATION.$RELEASE_NUMBER
-	RELEASE_FOLDER=${UPLOAD_AREA}${SW_RELEASE_FOLDER}
+	RELEASE_FOLDER=${UPLOAD_AREA_FOLDER}${SW_RELEASE_FOLDER}
 	SATELLITE_USER="efxbuild"
 	SATELLITE_PASSWD="RedHat01"
 	SATELLITE_EFX_CHANNELS_TMP=SATELLITE_EFX_CHANNELS.out
@@ -35,28 +39,34 @@ function upload2Satellite {
 	#List CAPLIN SATELLITE CHANNELS
 	$EFX_INSTALLER_HOME/caplinChannels.sh > $SATELLITE_CAPLIN_CHANNELS_TMP
 	
-	# Check 4 Release Folder
+	# Check 4 existence of Release Folder in the FS
 	if [ ! -d "$RELEASE_FOLDER" ]; then
-	  echo "Sorry You must create Directory $RELEASE_FOLDER first"
-	  exit 0
+	  echo "Sorry You must create Directory $RELEASE_FOLDER first" | tee --append $EFX_INSTALLER_ERROR_FILE
+	  exit 1
+	fi
+	
+	# Choose correct Channels depending on Application
+	if [ "$1" = "Cerebro" ] || [ "$1" = "Baxter" ] || [ "$1" = "Baxter_Air" ]; then
+		selectedSatelliteChannels=$SATELLITE_EFX_CHANNELS_TMP
+	else # Caplin 
+		selectedSatelliteChannels=$SATELLITE_CAPLIN_CHANNELS_TMP
 	fi
 	
 	# Upload to every Channel
-	echo "Uploading to Satellite EFX Channels as User: $SATELLITE_USER and Password: $SATELLITE_PASSWD for $1 $RELEASE_NUMBER..."
+	echo "Uploading $SW_RELEASE_FOLDER to Satellite EFX Channels as User: $SATELLITE_USER and Password: $SATELLITE_PASSWD..." | tee --append $EFX_INSTALLER_LOG_FILE
 	count=0	
 	while read CHANNEL
 	do
 		let count++
-		echo -e "\n"
-		echo "Uploading to Channel: $CHANNEL [$count]"
-		rhnpush -l --channel=$CHANNEL -d $RELEASE_FOLDER --newest --server=https://lnx-satellitep1.ants.ad.anplc.co.uk/APP -u $SATELLITE_USER  -p $SATELLITE_PASSWD
-	done < $SATELLITE_EFX_CHANNELS_TMP
+		echo -e "\nUploading to Channel: $CHANNEL [$count]..." | tee --append $EFX_INSTALLER_LOG_FILE
+		rhnpush --channel=$CHANNEL -d $RELEASE_FOLDER --newest --server=https://lnx-satellitep1.ants.ad.anplc.co.uk/APP -u $SATELLITE_USER  -p $SATELLITE_PASSWD >>$EFX_INSTALLER_LOG_FILE
+		rhnpush -l --channel=$CHANNEL -d $RELEASE_FOLDER --server=https://lnx-satellitep1.ants.ad.anplc.co.uk/APP -u $SATELLITE_USER  -p $SATELLITE_PASSWD | grep $RELEASE_NUMBER >>$EFX_INSTALLER_LOG_FILE
+	done < $selectedSatelliteChannels
 	
 	#remove temporary files
 	rm $SATELLITE_EFX_CHANNELS_TMP $SATELLITE_CAPLIN_CHANNELS_TMP
 	
-	echo -e "\n"
-	echo "Uploaded to $count Satellite EFX Channels as User: $SATELLITE_USER and Password: $SATELLITE_PASSWD for $1 $RELEASE_NUMBER"
+	echo -e "\n#########################################################################################"
+	echo -e "### Uploaded $1 $RELEASE_NUMBER to $count Satellite EFX Channels as User: $SATELLITE_USER and Password: $SATELLITE_PASSWD" | tee --append $EFX_INSTALLER_LOG_FILE
+	echo -e "#########################################################################################"
 }
-
-#upload2Satellite
